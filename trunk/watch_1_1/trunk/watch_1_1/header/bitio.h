0a5f5d877b6fc801a2d681c8cb203863f3b0aec2
reinhard0501 <reinhard0501@f38a9281-3bf9-fd7d-023a-a64bc6b7fefa>
2014-05-24 20:01:56 +0000

    first version

---
+++ trunk/watch_1_1/trunk/watch_1_1/header/bitio.h
@@ -0,0 +1,33 @@
+/**
+ *  BitIO.h
+ *	@author 	Andi Dittrich <http://andidittrich.de>
+ *	@version	1.0
+ *	@license	MIT Style X11 License
+*/
+ 
+ 
+#include <inttypes.h>
+ 
+#ifndef BITIO_H_
+#define BITIO_H_
+ 
+// set bit
+static inline void BIT_SET(volatile uint8_t *target, uint8_t bit) __attribute__((always_inline));
+static inline void BIT_SET(volatile uint8_t *target, uint8_t bit){
+	*target |= (1<<bit);
+};
+ 
+// set clear
+static inline void BIT_CLEAR(volatile uint8_t *target, uint8_t bit) __attribute__((always_inline));
+static inline void BIT_CLEAR(volatile uint8_t *target, uint8_t bit){
+	*target &= ~(1<<bit);
+};
+ 
+// bit toogle
+static inline void BIT_TOGGLE(volatile uint8_t *target, uint8_t bit) __attribute__((always_inline));
+static inline void BIT_TOGGLE(volatile uint8_t *target, uint8_t bit){
+	*target ^= (1<<bit);
+};
+ 
+ 
+#endif /* BITIO_H_ */
---
+++ trunk/watch_1_1/trunk/watch_1_1/header/watch_structs.h
@@ -0,0 +1,52 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 04.04.2014 22:02:40
+ *  Author: gazkuhl
+ */ 
+
+
+#ifndef WATCH_STRUCTS_H_
+#define WATCH_STRUCTS_H_
+
+/*defines PortA*/
+
+#define PA7     7
+#define PA6     6
+#define PA5     5
+#define PA4     4
+#define PA3     3
+#define PA2     2
+#define PA1     1
+#define PA0     0
+
+/*defines for 7-segment output*/
+
+#define SEG_0		0x08
+#define SEG_1		0x6e
+#define SEG_2		0x14
+#define SEG_3		0x44
+#define SEG_4		0x62
+#define SEG_5		0x41
+#define SEG_6		0x01
+#define SEG_7		0x6c
+#define SEG_8		0x0
+#define SEG_9		0x40
+#define SEG_ERROR	0x11
+
+
+typedef struct time_s 
+{
+	uint8_t seconds_l;
+	uint8_t seconds_h;
+	uint8_t minutes_l;
+	uint8_t minutes_h;	
+	uint8_t hours_l;
+	uint8_t hours_h;
+} time_t;
+
+
+
+
+
+#endif /* INCFILE1_H_ */
